ff5d45fa8c6a44afadf1fe800bad5600bdf6b01b
Jérémie Drouet <jeremie.drouet@gmail.com>
2024-07-16 21:20:54 +0200

    build: add libs
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- build.sh
+++ build.sh
@@ -6,4 +6,8 @@ dpkg --add-architecture arm64
 apt-get update
 apt-get install -y libssl-dev:arm64 gcc-aarch64-linux-gnu g++-aarch64-linux-gnu
 
+export OPENSSL_INCLUDE_DIR=/usr/include/aarch64-linux-gnu/openssl
+export OPENSSL_LIB_DIR=/usr/lib/aarch64-linux-gnu
+export PKG_CONFIG_PATH=/usr/lib/aarch64-linux-gnu/pkgconfig
+
 cargo build --release --target aarch64-unknown-linux-gnu
